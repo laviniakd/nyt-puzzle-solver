582e5a79c559c6d9774ade4d294cbe88a5c423f5
Lavinia Dunagan <laviniad@uw.edu>
2018-12-30 15:54:36 -0800

    Reorganize letter methods and comment

--- NYTSpellingBeeSolver/solver.java
+++ NYTSpellingBeeSolver/solver.java
@@ -8,15 +8,11 @@ public class solver {
 	
 	public static void main(String[] args) throws FileNotFoundException {
 		Scanner in = new Scanner(System.in);
-		System.out.println("Input the middle letter: ");
-		String middle = ("" + in.nextLine().charAt(0)).toLowerCase();
+		System.out.println("Input the middle letter: "); //requires English letter input
+		String middle = in.nextLine().toLowerCase();
 		
-		char[] alphabet = new char[26];
-		for (int i = 0; i < 26; i++) {
-			alphabet[i] = 'a' + i;
-		}	
-		
-		List<String> badLetters = makeBadLetters(in, alphabet);
+		List<String> goodLetters = getGoodLetters(in, middle);
+		List<String> badLetters = makeBadLetters(goodLetters);
 		
 		List<String> words = generate(middle, badLetters);
 		int num = words.size();
@@ -26,18 +22,13 @@ public class solver {
 		}
 	}
 	
-	public static List<String> makeBadLetters(Scanner in, char[] alphabet) {
-		System.out.println("Now input the other six letters: ");
-		String aux = in.nextLine().toLowerCase();
-		
-		while (aux.length() != 6) {
-			System.out.print("Try again!");
-			aux = in.nextLine();
-		}
-		
-		List<String> goodLetters = new ArrayList<String>();
-		for (int i = 0; i < 6; i++) {
-			goodLetters.add("" + aux.charAt(i));
+	//returns a list of the letters that can't be in the returned words
+	//List<String> goodLetters = available letters, not including the middle one
+	public static List<String> makeBadLetters(List<String> goodLetters) {
+		//creates an alphabet model
+		char[] alphabet = new char[26];
+		for (int i = 0; i < 26; i++) {
+			alphabet[i] = 'a' + i;
 		}
 		
 		List<String> badLetters = new ArrayList<String>();
@@ -51,6 +42,30 @@ public class solver {
 		return badLetters;
 	}
 	
+	//returns the available letters, not including the middle one
+	//requires entirely English letter input
+	//Scanner in = console for user input
+	//String middle = middle letter
+	public static List<String> getGoodLetters(Scanner in, String middle) {
+		System.out.println("Now input the other six letters: ");
+		String aux = in.nextLine().toLowerCase();
+		
+		while (aux.length() != 6 || aux.indexOf(middle) != -1) {
+			System.out.print("Try again!");
+			aux = in.nextLine();
+		}
+		
+		List<String> goodLetters = new ArrayList<String>();
+		for (int i = 0; i < 6; i++) {
+			goodLetters.add("" + aux.charAt(i));
+		}
+		
+		return goodLetters;
+	}
+	
+	//returns the list of accepted words
+	//String middle = middle letter
+	//List<String> badLetters = unavailable letters
 	public static List<String> generate(String middle, List<String> badLetters) 
 										throws FileNotFoundException {
 		List<String> words = new ArrayList<String>();
